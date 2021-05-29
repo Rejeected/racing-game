32551301b5f033003e9488db5c653c79e4a0aa64
Yuriy <72976802+Rejeected@users.noreply.github.com>
2021-05-29 11:30:09 +0500

    Add files via upload
    залил новые файлы игры

---
+++ Car Racing 2D Game/Properties/Resources.Designer.cs
@@ -0,0 +1,173 @@
+﻿//------------------------------------------------------------------------------
+//     Этот код создан программой.
+//     Исполняемая версия:4.0.30319.42000
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода.
+//------------------------------------------------------------------------------
+
+namespace Car_Racing_Game_MOO_ICT.Properties {
+    using System;
+    
+   
+    ///   Класс ресурса со строгой типизацией для поиска локализованных строк и т.д.
+    // Этот класс создан автоматически классом StronglyTypedResourceBuilder
+    // с помощью такого средства, как ResGen или Visual Studio.
+    // Чтобы добавить или удалить член, измените файл .ResX и снова запустите ResGen
+    // с параметром /str или перестройте свой проект VS.
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    internal class Resources {
+        
+        private static global::System.Resources.ResourceManager resourceMan;
+        
+        private static global::System.Globalization.CultureInfo resourceCulture;
+        
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources() {
+        }
+        
+        ///   Возвращает кэшированный экземпляр ResourceManager, использованный этим классом.
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Car_Racing_Game_MOO_ICT.Properties.Resources", typeof(Resources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+        
+        ///   Перезаписывает свойство CurrentUICulture текущего потока для всех
+        ///   обращений к ресурсу с помощью этого класса ресурса со строгой типизацией.
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
+                return resourceCulture;
+            }
+            set {
+                resourceCulture = value;
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap ambulance {
+            get {
+                object obj = ResourceManager.GetObject("ambulance", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap bronze {
+            get {
+                object obj = ResourceManager.GetObject("bronze", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap carGreen {
+            get {
+                object obj = ResourceManager.GetObject("carGreen", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap carGrey {
+            get {
+                object obj = ResourceManager.GetObject("carGrey", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap carOrange {
+            get {
+                object obj = ResourceManager.GetObject("carOrange", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap carPink {
+            get {
+                object obj = ResourceManager.GetObject("carPink", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap CarRed {
+            get {
+                object obj = ResourceManager.GetObject("CarRed", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap carYellow {
+            get {
+                object obj = ResourceManager.GetObject("carYellow", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap explosion {
+            get {
+                object obj = ResourceManager.GetObject("explosion", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap gold {
+            get {
+                object obj = ResourceManager.GetObject("gold", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.IO.UnmanagedMemoryStream, аналогичного System.IO.MemoryStream.
+        internal static System.IO.UnmanagedMemoryStream hit {
+            get {
+                return ResourceManager.GetStream("hit", resourceCulture);
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap roadTrack {
+            get {
+                object obj = ResourceManager.GetObject("roadTrack", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap silver {
+            get {
+                object obj = ResourceManager.GetObject("silver", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap TruckBlue {
+            get {
+                object obj = ResourceManager.GetObject("TruckBlue", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        internal static System.Drawing.Bitmap TruckWhite {
+            get {
+                object obj = ResourceManager.GetObject("TruckWhite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+    }
+}
